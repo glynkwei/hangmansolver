771ebcf2cde6c71b2ae41b3b4e447d912022671a
Glyn Wei <glynk.wei@gmail.com>
2018-04-26 03:28:40 -0400

    Added async processing, progress bar

--- src/app/Dictionary.js
+++ src/app/Dictionary.js
@@ -57,21 +57,56 @@ const createCharMap = (word : string | Hint) => {
     return charMap;
 };
 
+// Asynchronously process 10,000 words at a time
+const BATCH_SIZE = 1000;
+
 export class Dictionary {
     wordMap: Map<string, Map<string, Set<Letter>>>;
 
-    constructor(words?: Array<string>) {
+    constructor(words?: Array<string>, onProgress: number => void) {
         this.wordMap = new Map();
         if (words !== undefined) {
-            this.add(words);
+            this.add(words, onProgress);
         }
     }
 
-    add = (words: Array<string>) => {
-        for (let word of words) {
+    add = (words: Array<string>, onProgress: number => void, prevState: {index: number, progress: number} = {}) => {
+        let {progress = 0, index = 0} = prevState;
+        let i = index;
+        for (let k = 0; i < words.length && k < BATCH_SIZE; i++, k++) {
+            if (i >= Math.floor(progress * words.length / 100) - 1)
+            {
+                progress += Math.min(1, Math.ceil(100 / words.length));
+                if (onProgress)
+                {
+                    onProgress(progress);
+                }
+            }
+            const word = words[i];
             const charMap = createCharMap(word);
             [...charMap.entries()].forEach((entry) => {
                const key = getKey(entry[0], entry[1], word.length);
+                const wordSet = this.wordMap.get(key) || new Map();
+                wordSet.set(word, new Set(charMap.keys()));
+                this.wordMap.set(key, wordSet);
+            });
+        }
+        if (i !== words.length) {
+            setTimeout(() => this.add(words,onProgress, {index: i, progress}));
+        }
+    };
+
+    processData = (words: Array<string>, index: number, onProgress: number => void) => {
+        let progress = 0;
+        for (let i = index; i < words.length; i++) {
+            if (i >= Math.floor(progress * words.length / 100) - 1)
+            {
+                progress += Math.min(1, Math.ceil(100 / words.length));
+            }
+            const word = words[i];
+            const charMap = createCharMap(word);
+            [...charMap.entries()].forEach((entry) => {
+                const key = getKey(entry[0], entry[1], word.length);
                 const wordSet = this.wordMap.get(key) || new Map();
                 wordSet.set(word, new Set(charMap.keys()));
                 this.wordMap.set(key, wordSet);
--- src/app/HangmanView.js
+++ src/app/HangmanView.js
@@ -5,35 +5,61 @@ import words from '../data/words.json';
 import {Dictionary, parseStringAsHint} from './Dictionary';
 import AlphabetView from './AlphabetView';
 import SolutionView from './SolutionView';
-import {Header,Segment} from 'semantic-ui-react';
+import {Header,Segment,Progress, Modal} from 'semantic-ui-react';
 
 import styles from './HangmanView.css'
 
-type State = {hintInput: string};
-
-/* eslint-disable react/no-multi-comp */
-/* Heads up! HomepageHeading uses inline styling, however it's not the best practice. Use CSS or styled components for
- * such things.
- */
-
-
-
+type State = {
+    hintInput: string,
+    duds: Set<string>,
+    progress: number,
+    boundingRect: {
+        width: number,
+        height: number,
+        top: number,
+        left: number}
+};
 
 export default class extends Component<{}, State> {
     dictionary: Dictionary;
     constructor() {
         super();
-        this.dictionary = new Dictionary(words.data);
-        this.state = {hintInput: '', duds: new Set()};
+        this.dictionary = new Dictionary();
+        this.state = {
+            hintInput: '',
+            boundingRect:{
+                width: 0,
+                height: 0,
+                top: 0,
+                left: 0
+            },
+            duds: new Set(),
+            progress: 0};
+    }
+
+    componentDidMount() {
+        this.dictionary.add(words.data, this.setProgress);
+        this.setState({boundingRect: this.hintRef.getBoundingClientRect()});
     }
 
     setHint = value => this.setState({hintInput: value});
     setDuds = duds => this.setState({duds});
-
+    setProgress = progress => this.setState({progress});
+    handleHintRef = (ref) => this.hintRef = ref;
+    handleProgressRef = ref => this.progressRef = ref;
     render() {
         const hint = parseStringAsHint(this.state.hintInput);
         return (
                 <React.Fragment>
+                    {this.state.progress < 100 &&
+                        <Modal defaultOpen closeOnDimmerClick={false}>
+                            <Modal.Content>
+                            <Progress value={this.state.progress} total={100} indicating precision={0} progress='percent'>
+                                Loading data...
+                            </Progress>
+                            </Modal.Content>
+                        </Modal>
+                    }
                     <Segment inverted textAlign='center' className='segment' vertical>
                         <Header
                             as='h1'
@@ -43,7 +69,9 @@ export default class extends Component<{}, State> {
                         />
                     </Segment>
                     <div className ='container'>
-                    <HintInput onChange={this.setHint}/>
+                    <div ref={this.handleHintRef}>
+                        <HintInput onChange={this.setHint} />
+                    </div>
                     <AlphabetView hint={hint} onChange={this.setDuds}/>
                     <SolutionView solutions={this.dictionary.search(hint, this.state.duds)}
                               limit={20}/>
